1c34f5a558a4d41113a5f45fe2caf5eb4d16a126
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-05 18:42:22 +0000

    Handle negative and greater than 0 case

--- solver.rb
+++ solver.rb
@@ -2,6 +2,10 @@ class Solver
     def factorial(n)
         if n < 0
             raise "factorial is not defined for negative integers"
+        elsif n == 0
+            1
+        else
+            (1..n).reduce(:*)
         end
     end
 end
